2b3830525a61c9e0100c4dcf22cd0edeea87b237
JHpusd <69809321+JHpusd@users.noreply.github.com>
2021-09-14 22:42:13 +0000

    made second test case

--- 002/test_decision_tree.py
+++ 002/test_decision_tree.py
@@ -4,7 +4,7 @@ from decision_tree import *
 
 init_points = {'x': [(1,1), (1,2), (2,2), (2,3)], 'o': [(1,3), (1,4), (2,4)]}
 dt = DecisionTree(init_points)
-
+print("first test case")
 print("initial points:", dt.point_dict)
 print("initial entropy:", dt.entropy)
 print("best split:", dt.get_best_split())
@@ -13,3 +13,16 @@ dt.fit()
 print("fitted the tree")
 print("prediction for (2,1):", dt.predict((2,1)))
 print("prediction for (1,6):", dt.predict((1,5)))
+
+init_points = {'o':[(2,1),(2,2),(3,1),(3,2)], 'x':[(3,3),(4,1),(4,2),(4,3),(5,1)]}
+dt = DecisionTree(init_points)
+print('\nsecond test case')
+print("initial points:",dt.entropy)
+print("best split:",dt.get_best_split())
+
+dt.fit()
+print("fitted the tree")
+print("prediction for (1,1):", dt.predict((1,1)))
+print("prediction for (2,3):", dt.predict((2,3)))
+print("prediction for (4,4):", dt.predict((4,4)))
+print("prediction for (5,2):", dt.predict((5,2)))
